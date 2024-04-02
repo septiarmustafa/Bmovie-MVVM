ab615dbe1218b51485febfeb1c777eafa325df5f
septiarmustafa <septiarmustafa@gmail.com>
2024-04-02 20:34:38 +0700

    api config

--- app/src/main/java/com/ansorisan/movieku_kt/api/ApiConfig.kt
+++ app/src/main/java/com/ansorisan/movieku_kt/api/ApiConfig.kt
@@ -1,11 +1,19 @@
 package com.ansorisan.movieku_kt.api
 
-import androidx.viewbinding.BuildConfig
+import com.ansorisan.movieku_kt.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class ApiConfig {
     companion object {
-//        fun getApiService(): ApiService {
-//            val loggingInterceptor = if(BuildConfig.DEBUG)
-//        }
+        fun getApiService(): ApiService {
+            val loggingInterceptor = if(BuildConfig.DEBUG) HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+            else HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
+            val client = OkHttpClient.Builder().addInterceptor(loggingInterceptor).build()
+            val retrofit = Retrofit.Builder().baseUrl(BuildConfig.BASE_URL).addConverterFactory(GsonConverterFactory.create()).client(client).build()
+            return retrofit.create(ApiService::class.java)
+        }
     }
 }
---
+++ app/src/main/java/com/ansorisan/movieku_kt/api/RequestState.java
@@ -0,0 +1,4 @@
+package com.ansorisan.movieku_kt.api;
+
+public class RequestState {
+}
