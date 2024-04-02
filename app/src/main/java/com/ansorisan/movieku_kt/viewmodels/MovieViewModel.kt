699da4c44bdb13016e24e1a2f2ad7e6a716434b8
septiarmustafa <septiarmustafa@gmail.com>
2024-04-02 20:57:42 +0700

    movie view model

--- app/src/main/java/com/ansorisan/movieku_kt/viewmodels/MovieViewModel.kt
+++ app/src/main/java/com/ansorisan/movieku_kt/viewmodels/MovieViewModel.kt
@@ -1,10 +1,24 @@
 package com.ansorisan.movieku_kt.viewmodels
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import com.ansorisan.movieku_kt.api.RequestState
+import com.ansorisan.movieku_kt.models.MovieResponse
 import com.ansorisan.movieku_kt.repositories.MovieRepository
+import retrofit2.HttpException
 
 class MovieViewModel: ViewModel() {
     private val repo: MovieRepository = MovieRepository()
+    private val page = 1
 
-
+    fun getPopularMovie(): LiveData<RequestState<MovieResponse?>> = liveData {
+        emit(RequestState.Loading)
+        try {
+            val response = repo.getPopularMovie(page)
+            emit(RequestState.Success(response))
+        } catch (e: HttpException) {
+            e.response()?.errorBody()?.string()?.let { RequestState.Error(it) }?.let{emit(it)}
+        }
+    }
 }
